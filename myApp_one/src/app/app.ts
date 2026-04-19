8d56fae2318034cd1e60a76392ceec9fab518127
Kashukartik <kashukartik21@gmail.com>
2026-04-19 11:14:02 +0530

    updated

--- myApp_one/src/app/app.ts
+++ myApp_one/src/app/app.ts
@@ -1,12 +1,19 @@
 import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common'
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('myApp_one');
+
+  courses = [
+    {id: 1, name: "C1"},
+    {id: 2, name: "C2"},
+    {id: 3, name: "C3"},
+    {id: 4, name: "C4"}, 
+  ];
 }
