c4d36890e61fcb0a77e2d2e4fca2e18233b1283f
Kashukartik <kashukartik21@gmail.com>
2026-04-19 11:46:14 +0530

    U

--- myApp_one/src/app/app.ts
+++ myApp_one/src/app/app.ts
@@ -11,9 +11,17 @@ import { RouterOutlet } from '@angular/router';
 export class App {
 
   courses = [
-    {id: 1, name: "C1"},
-    {id: 2, name: "C2"},
-    {id: 3, name: "C3"},
-    {id: 4, name: "C4"}, 
+    {id: 1, name: "Course 1"},
+    {id: 2, name: "Course 2"},
+    {id: 3, name: "Course 3"},
+    {id: 4, name: "Course 4"}, 
   ];
+
+  onAdd(){
+    this.courses.push({id: 5, name: "Course 5"})
+  }
+
+  onRemove(index: number){
+    this.courses.splice(index, 1);
+  }
 }
