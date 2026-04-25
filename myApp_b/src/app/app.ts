fda10d9c77eb16da842dfc2684d4a1b5ff510310
Kashukartik <kashukartik21@gmail.com>
2026-04-25 17:01:07 +0530

    Update_a

--- myApp_b/src/app/app.ts
+++ myApp_b/src/app/app.ts
@@ -1,11 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home-component/home-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HomeComponent 
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  
 })
 export class App {
   protected readonly title = signal('myApp_b');
